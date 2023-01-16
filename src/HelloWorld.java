f3412be85c4f076dfc4499ae8f44be061e39462d
Victor Pena <13victor.pena@gmail.com>
2023-01-16 12:04:25 -0600

    updated java class name from app to HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("code inside of curly braces should be indented");
+        System.out.println("with four spaces");
+        // I am a single line comment
+        /* i take up one line */
+        /**
+         * i take up
+         * multiple
+         * lines
+         */
+    }
+}
