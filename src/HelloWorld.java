b5bbd651cf422e977447a7e31ecee16eccfb97b5
Victor Pena <13victor.pena@gmail.com>
2023-01-16 12:29:41 -0600

    variables must be declared by type. introduced byte and short

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("Hello, World!");
         System.out.println("code inside of curly braces should be indented");
         System.out.println("with four spaces");
+
         // I am a single line comment
         /* i take up one line */
         /**
@@ -10,5 +12,17 @@ public class HelloWorld {
          * multiple
          * lines
          */
+
+        int id;
+
+        byte byteRangeFrom = -128;
+        byte byteRangeTo = 127;
+        short shortRangeFrom = -32768;
+        short shortRangeTo = 32767;
+
+        System.out.println("byte range from " + byteRangeFrom + " to " + byteRangeTo);
+
+
+
     }
 }
