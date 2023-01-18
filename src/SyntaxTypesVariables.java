edba3e10675746c192206714d61b10c7591cc8ea
Victor Pena <13victor.pena@gmail.com>
2023-01-18 13:28:54 -0600

    added comments

--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -20,6 +20,12 @@ public class SyntaxTypesVariables {
         int a = 5;
         System.out.println(a++);
         System.out.println(a);
+
+        int b = 5;
+        System.out.println(++b);
+        System.out.println(b);
+
+
         
 
         
