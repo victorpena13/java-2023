8a7491b0b65fbb5ec7e620015fef28fca4097f58
Victor Pena <13victor.pena@gmail.com>
2023-01-18 13:26:58 -0600

    casting | increments and decrements

--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -1,13 +1,26 @@
 public class SyntaxTypesVariables {
-    
     public static void main(String[] args) {
         int myFavoriteNumber = 13; 
         System.out.println(myFavoriteNumber);
 
         String myString = "this is a string value";
         System.out.println(myString);
-        myString = 'a';
+        // myString = 'a'; // mismatch: cannot convert from char to string. 
+        myString = "a";
         System.out.println(myString);
+        // myString = 3.14159; //mismatch: required string gave double
+        float myNumber;
+        // System.out.println(myNumber); // has not been intitialized.
+        myNumber = 123L;
+        myNumber = 123; 
+        System.out.println(myNumber);
+        myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int a = 5;
+        System.out.println(a++);
+        System.out.println(a);
+        
 
         
 
