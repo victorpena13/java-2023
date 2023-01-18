4831a444539385a08ab07b0b990469d3e6a4b95c
Victor Pena <13victor.pena@gmail.com>
2023-01-18 16:42:17 -0600

    used format to display two decimal places of pi

--- src/ConsoleInputOutput.java
+++ src/ConsoleInputOutput.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class ConsoleInputOutput {
 
     public static void main(String[] args) {
         String name = "victor";
         System.out.printf("Hello there, %s. Nice to see you. %n", name);
+
+        String greeting = "salutations";
         
+        System.out.printf("%s, %s!", greeting, name);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter something: ");
+        String userInput = scanner.next();
+        System.out.println("You entered: ==> \"" + userInput + "\" <--");
+        
+
+
 
     }
     
