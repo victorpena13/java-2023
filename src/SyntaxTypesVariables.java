2d9d460d62de17aad83b80e6ba11df590dc12dab
Victor Pena <13victor.pena@gmail.com>
2023-01-17 23:48:31 -0600

    single and double qoutations

--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -4,5 +4,12 @@ public class SyntaxTypesVariables {
         int myFavoriteNumber = 13; 
         System.out.println(myFavoriteNumber);
 
+        String myString = "this is a string value";
+        System.out.println(myString);
+        myString = 'a';
+        System.out.println(myString);
+
+        
+
     }
 }
