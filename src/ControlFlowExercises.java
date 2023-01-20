9c4f817bd9be086f58f7e64efc298fa27d8b5955
Victor Pena <13victor.pena@gmail.com>
2023-01-20 10:40:23 -0600

    while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        while(a<=15) {
+            System.out.print(a+" ");
+            a++;
+        }
+
+    }
+    
+}
