80b76ea2e06db2aee23f0ffc5d064bf3593127f0
Victor Pena <13victor.pena@gmail.com>
2023-01-20 11:29:46 -0600

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -17,12 +17,34 @@ public class ControlFlowExercises {
 
         } while(b<=100);
 
-        int c = 100;
+        int c=100;
 
         do {
             System.out.println(c);
             c-=5;
         } while(c>=-10);
+
+       long d = 2;
+       do {
+           System.out.println(d);
+           d*= d;
+       } while (d < 1000000);
+
+
+       int e=1;
+
+       do {
+        if (e%3==0 && e%5==0) {
+            System.out.println("fizzbuzz");
+        }else if(e%3==0) {
+            System.out.println("fizz");
+        } else if(e%5==0) {
+            System.out.println("buzz");
+        } else {
+            System.out.println(e);
+        }
+        e++;
+       } while(e<=100);
 
     }
     
