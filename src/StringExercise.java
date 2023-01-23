ecd6aea1cbf27a3ebd207c1682a146778cbf50b0
Victor Pena <13victor.pena@gmail.com>
2023-01-22 18:53:52 -0600

    quotations inside of quotations

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    
+    public static void main(String[] args) {
+
+        String lyric = "we don't need no education\nwe don't need no thought control";
+        System.out.println(lyric);
+
+        String quotations = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotations);
+
+
+
+
+    }
+}
--- src/Strings.java
+++ src/Strings.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Strings {
 
     public static void main(String[] args) {
         String message = "hello";
-        System.out.println(message);
+        System.out.println(message); //hello
         message = message + "world";
-        System.out.println(message);
+        System.out.println(message); //hello world
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Continue? [y/n]");
+        String userInput = scanner.next();
+
+        userInput.equalsIgnoreCase(userInput);
+
+        String input = "i rock";
+        input.equalsIgnoreCase("test"); //false
+        input.equals("I Rock"); //false
+        input.equalsIgnoreCase("I Rock"); //true
+        input.equals("i rock"); //true
+        input.startsWith("i"); //true
+        input.startsWith("I"); //false
+        input.endsWith("rock"); //true
+        input.endsWith("Rock"); //false
+
     }
     
 }
