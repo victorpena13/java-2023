cd113cc3d286ed1c0c1c87db1c33fcceffe6364c
Victor Pena <13victor.pena@gmail.com>
2023-01-22 18:57:37 -0600

    backslashes

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,11 +2,13 @@ public class StringExercise {
     
     public static void main(String[] args) {
 
-        String lyric = "we don't need no education\nwe don't need no thought control";
-        System.out.println(lyric);
+        System.out.println("we don't need no education\nwe don't need no thought control");
 
-        String quotations = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(quotations);
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println("In windows, the main drive is usually c:\\");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!");
+        
+
 
 
 
