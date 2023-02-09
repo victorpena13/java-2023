0a868eeed3cc75216d982bd03aa74f9738bc3aa9
Victor Pena <13victor.pena@gmail.com>
2023-02-09 13:02:09 -0600

    shapes: inheritance and polymorphism

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,5 +5,17 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
     }
+
+    public double getArea() {
+        System.out.println("override: ");
+        double side = this.length;
+        return Math.pow(side, 2);
+    }
+
+    public double getPerimeter() {
+        System.out.print("override: ");
+        double side = this.length;
+        return 4 * side;
+    }
     
 }
